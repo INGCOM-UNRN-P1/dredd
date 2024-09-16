581e1c144169c9d2be35364a2325e16cc107bd0f
Martín René <martinvilu@gmail.com>
2024-09-16 11:07:55 -0300

    typo en la dirección del script

--- comment.sh
+++ comment.sh
@@ -4,7 +4,7 @@ echo "Procesando comentarios de corrección para $1"
 
 if [ -f "$1.md" ]; then
     gh pr comment https://github.com/INGCOM-UNRN-P1/$1/pull/1 -F $1.md >> comment.md
-    firefox -new-tab -url "https://github.com/INGCOM-UNRN-PII/$1/pull/1/files" -new-tab -url "``"
+    firefox -new-tab -url "https://github.com/INGCOM-UNRN-P1/$1/pull/1/files" -new-tab -url "``"
 else
     echo "El reporte '$1.md' no fue encontrado"
     echo "Parece que dredd no pasó por acá aún."
